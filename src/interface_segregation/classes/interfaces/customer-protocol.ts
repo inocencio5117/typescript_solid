08bd09b26be9f65abe6905bcddfe7e1de574eb16
Vinicius Inocencio <vicnicius2009@gmail.com>
2022-06-24 16:47:48 -0300

    Feat:  adding interface segregation principle

---
+++ src/interface_segregation/classes/interfaces/customer-protocol.ts
@@ -0,0 +1,10 @@
+export interface CustomerProtocol {
+  firstName: string;
+  lastName: string;
+  cpf: string;
+}
+
+export interface EnterpriseCustomerProtocol {
+  name: string;
+  cnpj: string;
+}
